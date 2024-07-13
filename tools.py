a791b02a2d1b829c00a1cf53144658073dba1290
Rory <16675082+roryeckel@users.noreply.github.com>
2024-07-13 15:35:09 -0500

    Support Valves

--- tools.py
+++ tools.py
@@ -1,10 +1,24 @@
+"""
+title: WolframAlpha API
+author: ex0dus
+author_url: https://github.com/roryeckel/open-webui-wolframalpha-tool
+version: 0.1.1
+"""
+
 import os
 import requests
+from pydantic import BaseModel, Field
 
 
 class Tools:
+    class Valves(BaseModel):
+        WOLFRAMALPHA_APP_ID: str = Field(
+            default="",
+            description="The App ID (api key) to authorize WolframAlpha",
+        )
+
     def __init__(self):
-        pass
+        self.valves = self.Valves()
 
     # def get_app_id(self) -> str:
     #     """
@@ -19,14 +33,13 @@ class Tools:
         The engine also supports textual queries stated in English about other topics.
         You should cite this tool when it is used. It can also be used to supplement and back up knowledge you already know.
         WolframAlpha can be used as a last resort when the answer to a question is unclear, or when real time data is required.
-        :param query_string: The question or mathematical equation to ask the WolframAlpha engine
+        :param query_string: The question or mathematical equation to ask the WolframAlpha engine. DO NOT use backticks or markdown when writing your JSON request.
         :return: A short answer or explanation of the result of the query_string
         """
-        app_id = os.getenv("WOLFRAMALPHA_APP_ID")
+        app_id = self.valves.WOLFRAMALPHA_APP_ID or os.getenv("WOLFRAMALPHA_APP_ID")
+        print(f"App ID = {app_id}")
         if not app_id:
-            return (
-                "App ID is not set in the environment variable 'WOLFRAMALPHA_APP_ID'."
-            )
+            return "You are required to report the following error message to the user: App ID is not set in the Valves or the environment variable 'WOLFRAMALPHA_APP_ID'."
 
         base_url = "http://api.wolframalpha.com/v1/result"
         params = {
