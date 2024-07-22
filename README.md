138c32bf90b1a26217f6bb50a40aff5a42a451ff
Rory <16675082+roryeckel@users.noreply.github.com>
2024-07-21 20:50:34 -0500

    Upgrade to enable simple API

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Provides an interface for LLMs to query the WolframAlpha short answer text API in Open WebUI. Useful for Ollama!
 
 First, set the environment variable of WOLFRAMALPHA_APP_ID with the app ID from your WolframAlpha developer portal and restart Open WebUI. Paste the contents of tools.py into your Open WebUI Workspace's Tools. Then, you can enable the tool for a query and ask the LLM about some real time information.
+
+![Example Screenshot](https://github.com/roryeckel/open-webui-wolframalpha-tool/blob/main/Example.png)
--- tools.py
+++ tools.py
@@ -7,7 +7,77 @@ version: 0.1.1
 
 import os
 import requests
+import urllib.parse
 from pydantic import BaseModel, Field
+from typing import Callable, Awaitable
+
+
+async def query_simple(
+    query_string: str, app_id: str, __event_emitter__: Callable[[dict], Awaitable[None]]
+) -> None:
+    base_url = "http://api.wolframalpha.com/v1/simple"
+    params = {"i": query_string, "appid": app_id}
+
+    result_url = f"{base_url}?{urllib.parse.urlencode(params)}"
+
+    await __event_emitter__(
+        {
+            "type": "message",
+            "data": {"content": f"![WolframAlpha Simple Result]({result_url})"},
+        }
+    )
+
+
+async def query_short_answer(
+    query_string: str, app_id: str, __event_emitter__: Callable[[dict], Awaitable[None]]
+) -> str:
+    base_url = "http://api.wolframalpha.com/v1/result"
+    params = {
+        "i": query_string,
+        "appid": app_id,
+        "format": "plaintext",
+    }
+
+    await __event_emitter__(
+        {
+            "data": {
+                "description": f"Performing WolframAlpha short answer query: {query_string}",
+                "status": "in_progress",
+                "done": False,
+            },
+            "type": "status",
+        }
+    )
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+        text_response = response.text
+
+        await __event_emitter__(
+            {
+                "data": {
+                    "description": f"WolframAlpha returned: {text_response}",
+                    "status": "complete",
+                    "done": True,
+                },
+                "type": "status",
+            }
+        )
+        return "WolframAlpha: " + text_response
+    except Exception as e:
+        print(e)
+        await __event_emitter__(
+            {
+                "data": {
+                    "description": f"Error: WolframAlpha returned {e}",
+                    "status": "complete",
+                    "done": True,
+                },
+                "type": "status",
+            }
+        )
+        return f"There was an error fetching WolframAlpha response. You are required to report the following message to the user: {str(e)}"
 
 
 class Tools:
@@ -15,6 +85,10 @@ class Tools:
         WOLFRAMALPHA_APP_ID: str = Field(
             default="",
             description="The App ID (api key) to authorize WolframAlpha",
+        )
+        ENABLE_SIMPLE_API: bool = Field(
+            default=True,
+            description="Specify if the query should use the simple API. This will return images from WolframAlpha. Can be used in combination with short answer.",
         )
 
     def __init__(self):
@@ -27,7 +101,9 @@ class Tools:
     #     """
     #     return os.getenv("WOLFRAMALPHA_APP_ID")
 
-    def perform_query(self, query_string: str) -> str:
+    async def perform_query(
+        self, query_string: str, __event_emitter__: Callable[[dict], Awaitable[None]]
+    ) -> str:
         """
         Query the WolframAlpha knowledge engine to answer a wide variety of complex mathematical formulas including trigonometry and differential equations.
         The engine also supports textual queries stated in English about other topics.
@@ -39,19 +115,21 @@ class Tools:
         app_id = self.valves.WOLFRAMALPHA_APP_ID or os.getenv("WOLFRAMALPHA_APP_ID")
         print(f"App ID = {app_id}")
         if not app_id:
+            await __event_emitter__(
+                {
+                    "data": {
+                        "description": f"Error: WolframAlpha APP_ID is not set",
+                        "status": "complete",
+                        "done": True,
+                    },
+                    "type": "status",
+                }
+            )
             return "You are required to report the following error message to the user: App ID is not set in the Valves or the environment variable 'WOLFRAMALPHA_APP_ID'."
 
-        base_url = "http://api.wolframalpha.com/v1/result"
-        params = {
-            "input": query_string,
-            "appid": app_id,
-            "format": "plaintext",
-        }
+        short_answer = await query_short_answer(query_string, app_id, __event_emitter__)
 
-        try:
-            response = requests.get(base_url, params=params)
-            response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
-            return "WolframAlpha: " + response.text
-        except Exception as e:
-            print(e)
-            return f"There was an error fetching WolframAlpha response. You are required to report the following message to the user: {str(e)}"
+        if self.valves.ENABLE_SIMPLE_API:
+            await query_simple(query_string, app_id, __event_emitter__)
+
+        return short_answer
